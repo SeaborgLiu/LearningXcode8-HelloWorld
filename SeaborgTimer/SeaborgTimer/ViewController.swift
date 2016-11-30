b996e1319c21eaeec5e4538e59f094b62d40ea9d
Seaborg <seaborg@live.cn>
2016-11-30 12:14:28 +0800

    Initial Create

---
+++ SeaborgTimer/SeaborgTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeaborgTimer
+//
+//  Created by 刘旭波 on 2016/11/30.
+//  Copyright © 2016年 SeaborgSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
